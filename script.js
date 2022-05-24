ae51bf1e50c0844bb86420c1ef605409dfeddaa9
Armando <armando.cristiano.1999@gmail.com>
2022-05-24 15:51:50 +0200

    Prima di cambiare device e uscire

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-/*TODO: Quando quambi il colore al color-picker
+/*
+TODO: Quando quambi il colore al color-picker
 fai cambiare anche il colore della classe .atcive, o
-direttamente della variabile black nel root css*/
-// UI
+direttamente della variabile black nel root css
 
+FIXME: non dimenticare che appena parte la pagina deve
+  essere settata una grid di 16 x 16 
+*/
+
+/*TODO:
+  1 - Iniziamo col creare i div;
+  
+
+
+*/
+
+// COMPONENT
 const colorChoose = document.querySelector("#color-picker");
 const btnColorMode = document.querySelector("#color-mode");
 const btnRainbowMode = document.querySelector("#rainbow-mode");
@@ -13,4 +25,27 @@ const sizeGrid = document.querySelector("#size-grid");
 
 const grid = document.querySelector("#grid");
 
-console.log(grid);
+// Prendiamo il valore del sizeGrid e lo facciamo comparire a schermo
+sizeGrid.addEventListener("input", change_sizeValue);
+sizeGrid.addEventListener("click", createGrid);
+
+function change_sizeValue() {
+  sizeValue.innerText = `${this.value} x ${this.value}`;
+}
+
+// Creazione della griglia
+function createGrid() {
+  // Rimuovi ogni volta tutti i child
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+
+  // Aggiungi div rispetto al value di sizeGrid
+  for (let i = 1; i <= sizeGrid.value; i++) {
+    let div = document.createElement("div");
+    grid.appendChild(div);
+  }
+}
+
+createGrid();
+createGrid();
