156cdcde251f2b04bee7b48e2c7db724d4455bed
Armando <armando.cristiano.1999@gmail.com>
2022-05-24 12:06:27 +0200

    Iniziato col file javascript

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 /*TODO: Quando quambi il colore al color-picker
 fai cambiare anche il colore della classe .atcive, o
 direttamente della variabile black nel root css*/
+// UI
+
+const colorChoose = document.querySelector("#color-picker");
+const btnColorMode = document.querySelector("#color-mode");
+const btnRainbowMode = document.querySelector("#rainbow-mode");
+const btnDelete = document.querySelector("#delete");
+const btnClear = document.querySelector("#clear");
+const sizeValue = document.querySelector("#size-value");
+const sizeGrid = document.querySelector("#size-grid");
+
+const grid = document.querySelector("#grid");
+
+console.log(grid);
